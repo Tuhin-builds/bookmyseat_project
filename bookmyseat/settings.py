792fb6c36cf9a5e7d242aef2eb7fd8020d5b45fa
Tuhin Sarma Biswas <tuhinsarma30@gmail.com>
2026-08-07 22:02:54 +0530

    Switch to Neon PostgreSQL database

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import os
 from dotenv import load_dotenv
 
@@ -85,10 +86,11 @@ WSGI_APPLICATION = 'bookmyseat.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
